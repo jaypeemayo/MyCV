0e86196eee97e41e32955efc071db2e6af533f95
jaype <jaypeemayo@gmail.com>
2017-01-15 16:33:30 +0800

    My Resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import '../../public/css/styles.css';
+import {Component, ViewEncapsulation} from '@angular/core';
+//import '../../public/css/styles.scss';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss', '../../public/css/styles.scss'],
+    encapsulation: ViewEncapsulation.None,
     // styleUrls: ['./app.component.css']
 })
 export class AppComponent { }
